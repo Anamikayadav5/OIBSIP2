4e8a6fb8b07a2e5ac3fd2538c8a3cc881a588747
Anamikayadav5 <91691701+Anamikayadav5@users.noreply.github.com>
2023-01-29 20:53:48 +0530

    portfolio

---
+++ portfolio/js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","NSS Volunteer","Professional Speaker","Backend Developer","Tools Master","Frontend Developer"],
+    typeSpeed:100,
+    BackSpeed:110,
+    loop:true
+})
